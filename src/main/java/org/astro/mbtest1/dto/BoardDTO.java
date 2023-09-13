b7ef6bc3b879bed1077286c483d6c863d743f49c
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-09-13 16:37:11 +0900

    230913

---
+++ src/main/java/org/astro/mbtest1/dto/BoardDTO.java
@@ -0,0 +1,14 @@
+package org.astro.mbtest1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BoardDTO {
+    
+}

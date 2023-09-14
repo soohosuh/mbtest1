4c0bd99098d575ce021c459235ce49b28415f82e
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-09-14 16:51:57 +0900

    230914

--- src/main/java/org/astro/mbtest1/dto/BoardDTO.java
+++ src/main/java/org/astro/mbtest1/dto/BoardDTO.java
@@ -1,5 +1,7 @@
 package org.astro.mbtest1.dto;
 
+import java.time.LocalDate;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +12,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardDTO {
-    
+    private Integer bno;
+    private String title;
+    private String content;
+    private String writer;
+    private String duedate;
+    private LocalDate modifyDate;
 }

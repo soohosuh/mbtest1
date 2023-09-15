43f56cf08ae97cbfbfdb62e5cb6e01783ea3d39c
soohosuh <128343379+soohosuh@users.noreply.github.com>
2023-09-15 20:39:27 +0900

    230915

--- src/main/java/org/astro/mbtest1/mappers/Mapper.java
+++ src/main/java/org/astro/mbtest1/mappers/Mapper.java
@@ -1,7 +1,11 @@
 package org.astro.mbtest1.mappers;
 
+import org.astro.mbtest1.dto.BoardDTO;
 
 public interface Mapper {
+
+    int insert(BoardDTO boardDTO);
+    
     
     public String time();
 
